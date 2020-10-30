939496acee151ee8c2f006e4ac1827d34a85de4c
rajkumar <rajkumarpandiyaraj@gmail.com>
2020-10-29 18:56:12 -0700

    Login form using react-formik

---
+++ src/component/Input.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Field, ErrorMessage} from 'formik';
+import TextError from './TextError';
+
+function Input(props) {
+    const {name, label, ...rest} = props;
+    return (
+        <div className='form-control'>
+            <label htmlFor={name}>{label}</label>
+            <Field id={name} name={name} placeholder={label} {...rest}/>
+            <ErrorMessage name={name} component={TextError}/>
+        </div>
+    )
+}
+
+export default Input
+
